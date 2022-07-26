d3796b19bf630dd5063b24bc7bb0d7bdf7ac3d82
Muhammad Asif <asif88383@gmail.com>
2022-07-26 17:55:42 +0500

    fix

--- components/CompanySignup/SideAnimation.tsx
+++ components/CompanySignup/SideAnimation.tsx
@@ -8,7 +8,7 @@ const SideAnimation = () => {
   }, []);
 
   return (
-    <div className="w-full h-full text-white bg-violet-100 drop-shadow-sm z-50 overflow-hidden">
+    <div className="h-full text-white bg-violet-100 drop-shadow-sm z-50">
       <lottie-player
         id="firstLottie"
         ref={uRef}
