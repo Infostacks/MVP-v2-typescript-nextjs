d09c36be673c9fa27d43f410c8d3e1c31c3ecc3c
Muhammad Asif <asif88383@gmail.com>
2022-10-04 17:15:07 +0500

    Company signup sideanimaiton issue resolved

--- components/CompanySignup/SideAnimation.tsx
+++ components/CompanySignup/SideAnimation.tsx
@@ -2,17 +2,13 @@ import React, { useEffect, useRef } from "react";
 
 const SideAnimation = () => {
   const uRef = useRef(null);
-  
+
   useEffect(() => {
     import("@lottiefiles/lottie-player");
   }, []);
 
   return (
-<<<<<<< HEAD
-    <div className="h-full text-white bg-violet-100 drop-shadow-sm z-50">
-=======
     <div className="w-full h-full drop-shadow-sm z-50 overflow-hidden">
->>>>>>> 878eedfc3538a4f344d8a625e076bb47765cbc32
       <lottie-player
         id="firstLottie"
         ref={uRef}
