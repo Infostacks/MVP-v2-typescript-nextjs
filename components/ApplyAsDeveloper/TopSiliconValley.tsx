26c6783ea75ba77901b5f8db128cc77981536e33
imharoonarshad <imharoonarshad@gmail.com>
2022-10-18 09:50:44 -0400

    Meaningful Msg 3

---
+++ components/ApplyAsDeveloper/TopSiliconValley.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HireTopDeveloper from "../Buttons/HireTopDeveloper";
+
+const TopSiliconValleyCard = () => {
+  return (
+    <div className="drop-shadow-md shadow-lg shadow-cyan-200 flex flex-col border-gray-700 border-8 items-center justify-evenly h-96 w-3/4 bg-gray-900 rounded-3xl">
+      <div className="text-slate-400 fontsize text-4xl max-w-screen-2xl text-center w-3/4">
+        Access to top silicon valley companies, certified for life, and income
+        stability.
+      </div>
+      <HireTopDeveloper />
+    </div>
+  );
+};
+
+export default TopSiliconValleyCard;
--- components/SimplePricing/SimplePricing.tsx
+++ components/SimplePricing/SimplePricing.tsx
@@ -3,22 +3,12 @@ import HireTopDeveloper from "../Buttons/HireTopDeveloper";
 import FaQs from "../FaQs/FaQs";
 import TopDevelopers from "../TopDevelopers/TopDevelopers";
 import TrustedBy from "../TrustedBy/TrustedBy";
+import SimplePricingHeading from "./SimplePricingHeading";
 
 const SimplePricing = () => {
   return (
-    <div className="flex flex-col items-center w-full max-w-screen-2xl my-20 gap-32">
-      <div className="flex flex-col items-center w-full max-w-screen-2xl ">
-        <div className="text-center">
-          <h1 className="fontsize2 fontsize text-slate-400 pt-52 p-3">
-            Simple pricing
-          </h1>
-          <p className="text-2xl text-gray-400 max-w-3xl">
-            The rate you see on the developer profiles is the rate you pay. No
-            extra fees. The average rate for a developer is{" "}
-            <span className=" text-3xl font-bold">$34/hour</span>
-          </p>
-        </div>
-      </div>
+    <div className="flex flex-col items-center w-full max-w-screen-lg my-20 gap-36">
+      <SimplePricingHeading />
       <HireTopDeveloper />
       <TrustedBy />
       <FaQs />
